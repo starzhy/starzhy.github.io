ac6a5d5d8f229884e003d1ec67e242be67090bea
ZHY <zhy@meitu.com>
2016-01-13 16:42:32 +0800

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,170 +1,56 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { Router, Route, Link, IndexLink, browserHistory } from 'react-router'
-import Loading from './loading'
-import PageIndex from './PageIndex'
-import PagePlay from './PagePlay'
-import PageResult from './PageResult'
+import Index from './index'
+import About from './about'
 
-  
-var handleTimer;
 class App extends React.Component{
     constructor(props) { 
       super(props);
-      var hash =  window.location.hash.substr(1);
-      //根据hash来设置active
       this.state = {
-        route: window.location.hash.substr(1),
-        loaded:false,
-        active:'pageIndex',
-        mission:1,
-        timer:10,
-        gameover:false,
-        total:10
+        
       }
     }
     componentDidMount() {
-      window.addEventListener('hashchange', () => {
-        this.setState({
-          route: window.location.hash.substr(1)
-        });
-      })
-    }
-
-    handlePageLoaded(){
-      this.setState({
-        loaded:true
-      });
-    }
-
-    startGame(){
-      //数据重置
-      //location.hash ='#/play'
-      this.setState({
-        active:'pagePlay',
-        mission:1,
-        timer:10,
-        gameover:false
-      });
-
-      this.timerFunction();
       
     }
-    nextMission(){
-      var mission = this.state.mission+1;
-      if(mission>this.state.total){
-          this.setState({
-              mission:this.state.total,
-          });
 
-          this.switchPage('pageResult');
-          return false;
-      }
-      this.setState({
-          mission:mission,
-          timer:10
-      });
-    }
-
-    timerFunction(){
-      handleTimer = setInterval(()=>{
-          var timer = this.state.timer-1;
-          if(timer<0){
-              //游戏结束
-              this.setState({
-                  gameover:true
-              })
-              clearInterval(handleTimer);
-              return;
-          }
-          this.setState({
-              timer:timer
-          })
-      },1000)
-    }
-
+    //切换页面
     switchPage(page){
-      this.setState({
-        active:'pageResult',
-        mission:1,
-        timer:10
-      });
-
+      this.setState({ active:page });
     }
 
     render(){
         return (
-            <div className="ui-section">
-              <Loading  updatePageLoaded={this.handlePageLoaded.bind(this)}/>
-              <PageIndex 
-                loaded  = {this.state.loaded} 
-                current = {this.state.active=='pageIndex' ? true : false}
-                startGame = {this.startGame.bind(this)}
-              >
-              </PageIndex>
-
-
-              <PagePlay 
-                loaded  = {this.state.loaded} 
-                current = {this.state.active=='pagePlay' ? true : false}
-                startGame = {this.startGame.bind(this)} switchPage={this.switchPage.bind(this)}
-                timer = {this.state.timer} gameover={this.state.gameover}
-                timerFunction = {this.timerFunction}
-                nextMission ={this.nextMission.bind(this)}
-                mission = {this.state.mission}
-              >
-              </PagePlay>
-              <PageResult current = {this.state.active=='pageResult' ? true : false}
-                startGame = {this.startGame.bind(this)}
-              >
-              </PageResult> 
+            <div className="wrapper">
+              <Index />
             </div>
           )
     }
 }
 
 
-const Index = React.createClass({
+const NoMatch = React.createClass({
     render() { 
       return (
         <div>
-          <h2>Index!</h2>
+          <h2>404</h2>
         </div>
       )
     }
 })
 
 
-class Play extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Play</h2>
-        {this.props.children}
-      </div>
-    )
-  }
-}
 
 
-class Result extends React.Component {
-  render() {
-    return (
-      <div>
-        <h3>Result</h3>
-      </div>
-    )
-  }
-}
 
 
-React.render((
-  // <Router history={browserHistory}>
-  <Router>
-    <Route path="/" component={App}>
-      <Route path="/index" component={Index}></Route>
-      <Route path="/play" component={Play}></Route>
-      <Route path="/result" component={Result}></Route>
-    </Route>
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}></Route>
+    <Route path="index" component={Index}/>
+    <Route path="about" component={About}/>
+    <Route path="*" component={NoMatch}/>
   </Router>
 ), document.getElementById('container'))
 
